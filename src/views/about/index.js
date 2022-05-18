1facbdb6f04ce7977ca5cf3c011a56d2544a44ee
Alex fernandes <alex.soares1414@gmail>
2022-05-18 20:31:04 -0300

    nova rota /about

---
+++ src/views/about/index.js
@@ -0,0 +1,35 @@
+let About = {
+    render: async () => {
+      let view = `
+        <div>
+          <h1>About</h1>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea perspiciatis, ab nihil sapiente, incidunt totam iure iusto quaerat distinctio voluptatum quod tenetur saepe sint labore blanditiis atque minima ut error.
+  Quod obcaecati assumenda, inventore similique sunt quo, nihil nisi optio minima facilis soluta ipsum laudantium perferendis dolore ullam itaque pariatur quibusdam maxime minus impedit rerum? Consectetur sint iusto commodi iste?
+  Soluta perspiciatis odit nihil ut dignissimos sapiente, obcaecati reiciendis vel quod, eveniet architecto dolorem quaerat maiores quis maxime accusamus consectetur reprehenderit necessitatibus neque nostrum excepturi. Error iusto accusamus voluptatem fugiat.
+  Molestias aperiam nisi qui, quae neque repudiandae alias similique? Beatae vel necessitatibus eligendi voluptas soluta ipsa itaque architecto voluptatum error? Deserunt delectus accusantium commodi facilis, a ullam sint explicabo autem.
+  Quae repudiandae repellendus molestias! Maxime iure temporibus illum quas ad rerum soluta placeat beatae voluptatem voluptas repellat ipsum expedita sequi, perspiciatis doloremque quibusdam? A non ullam nisi debitis nemo necessitatibus?
+  Officia dolore, impedit a placeat optio possimus aperiam nesciunt. Nostrum, cum? Nobis molestiae odit, veniam mollitia unde voluptatem hic, repudiandae vel doloribus blanditiis soluta dolores quasi asperiores quidem numquam vero?
+  Eum distinctio praesentium aperiam qui quibusdam quae cupiditate quo nemo, eveniet ea iusto corporis officia numquam asperiores at nulla atque laboriosam, quaerat consequuntur fugiat aliquam sint, excepturi est harum. Suscipit?
+  Tempore quod sequi aspernatur! Iste aspernatur, expedita veniam similique, totam ullam voluptatem et mollitia tenetur eaque minus alias voluptas. Et quos dignissimos odit delectus facere consectetur dolorum totam culpa incidunt!
+  Eveniet minima, neque accusantium pariatur fuga voluptas tenetur nam eos beatae eligendi blanditiis inventore. At optio cum magnam recusandae numquam, nisi commodi tempora modi, ex quisquam molestiae repudiandae. Amet, ipsa?
+  Delectus ea corporis soluta esse tenetur laudantium, doloremque reprehenderit, nulla iste cum quisquam nostrum placeat! Unde laudantium quam excepturi nobis provident nam maxime, minima ex explicabo quod iste tenetur itaque?
+  Ducimus, illum, nisi asperiores, quos quod sunt qui laboriosam dolorem aut corrupti recusandae veritatis dignissimos nobis. Vel possimus impedit distinctio ipsum quaerat incidunt ex, quos sapiente commodi error, nesciunt praesentium?
+  Quos consectetur est dolorem hic in dicta corrupti excepturi culpa itaque, sed enim consequuntur et ipsam sequi illum optio quaerat dolorum deserunt quas tempore? Magnam quisquam ab accusamus excepturi reprehenderit.
+  Nesciunt eius, minima ab ea blanditiis perspiciatis a perferendis tempora veniam laboriosam est velit incidunt? Fugit delectus dolores, exercitationem id illo eligendi pariatur, quibusdam accusantium blanditiis cumque, officiis fuga eius.
+  Fuga nihil accusantium autem in explicabo molestiae nostrum, vero laborum maxime perspiciatis delectus vitae magnam at quis, corporis obcaecati dolore itaque! Ad architecto ducimus delectus esse distinctio autem a suscipit.
+  In unde beatae porro magni, omnis minima repellat nesciunt eos, distinctio dolores error obcaecati nihil, dignissimos quod! Voluptates placeat quidem nam eveniet, voluptas dolore quia non facilis veritatis. Aliquid, sequi?
+  Excepturi, saepe, ipsa repellat odit aspernatur dolores soluta porro distinctio aut recusandae numquam assumenda, error ut consequuntur. Illo molestiae a in quia sint quibusdam nihil tempore, quisquam laborum. Cupiditate, expedita.
+  Doloribus exercitationem alias nihil quasi repudiandae? Aspernatur magnam distinctio tempore doloribus, vel accusamus magni ratione esse maxime nihil tempora praesentium. Accusantium, aliquam reprehenderit. Neque nisi ullam perspiciatis ab ipsa. Et!
+  Amet ipsa delectus sit, ipsam temporibus obcaecati repellendus molestias quibusdam suscipit eligendi iusto magni consectetur? Inventore expedita magnam praesentium? Nesciunt laborum vel tempora. Minima tempore beatae ducimus inventore atque doloribus?
+  Obcaecati, similique? Natus, asperiores! Saepe omnis est illum consequuntur aperiam voluptates nam accusamus odit asperiores? Molestias porro soluta, a repellat laudantium, commodi reiciendis ex voluptatum fugiat dolore aspernatur! Sunt, consequuntur!
+  Soluta repellendus libero sed nisi reprehenderit animi sint dolor nihil ipsum quidem ea fuga nemo odit sequi dicta minima repudiandae corrupti facilis maxime ad quas porro, consequatur eligendi. Tempore, at.</p>
+        </div>
+      `
+      return view
+    },
+    after_render: async () => {}
+  };
+  
+  export default About;
+
+  
