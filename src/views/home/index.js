70715a77772316d7656f6163471369bb43977bba
douglas.morais@i-value.com.br <douglas.morais@i-value.com.br>
2022-05-19 21:42:16 -0300

    update sass

--- src/views/home/index.js
+++ src/views/home/index.js
@@ -1,12 +1,20 @@
 import Nav from '../../components/nav';
 import Footer from '../../components/footer';
+import ImagemQualquer from '../../img/logo.jpeg'
 
 let Home = {
   render: async () => {
     let view = `
       ${Nav}
-      <div>
+      <div class="container">
         <h1>Home page</h1>
+
+        <div class="card">
+          <p class="__title">Este é o meu CARD</p>
+          <img class="image" src="${ImagemQualquer}" alt="">
+          <button class="button _success">Clique aqui que nada acontece</button>
+        </div>
+
       </div>
       ${Footer}
     `
